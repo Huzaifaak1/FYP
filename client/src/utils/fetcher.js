354d2b32045e805c0096f590d76e9cb74fc6dce8
Huzaifa Ali Khan <108281380+Huzaifaak1@users.noreply.github.com>
2024-08-13 21:56:34 +0500

    removed nodemods and packagelock from client and changed axios

--- client/src/utils/fetcher.js
+++ client/src/utils/fetcher.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
-
-
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
-    withCredentials : true
+  baseURL: "https://fyp-be-theta.vercel.app/api",
+  withCredentials: true,
 });
 
 export default api;
